e8b7f20e7065358f1d1e4f7e3c019fab4743c055
root <root@localhost.localdomain>
2012-10-21 08:48:19 +0530

    Updated string resources and layout.

--- gen/com/onloop/post/R.java
+++ gen/com/onloop/post/R.java
@@ -29,9 +29,12 @@ public final class R {
     }
     public static final class string {
         public static final int app_name=0x7f040000;
-        public static final int hello_world=0x7f040001;
-        public static final int menu_settings=0x7f040002;
-        public static final int title_activity_main=0x7f040003;
+        public static final int menu_settings=0x7f040005;
+        public static final int postEntry=0x7f040001;
+        public static final int postProgress=0x7f040003;
+        public static final int postResponse=0x7f040004;
+        public static final int postSend=0x7f040002;
+        public static final int title_activity_main=0x7f040006;
     }
     public static final class style {
         public static final int AppTheme=0x7f050000;
