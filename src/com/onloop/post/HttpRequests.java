714ce3da068c1f1ca06329fca9835da71195e962
Pranav Ravichandran <me@onloop.net>
2013-12-26 22:22:38 +0530

    Retabbed #2

--- src/com/onloop/post/HttpRequests.java
+++ src/com/onloop/post/HttpRequests.java
@@ -20,46 +20,46 @@ import android.util.Base64;
  * Helper class with methods for HTTP POST and GET requests.
  */
 public class HttpRequests {
-	private HttpClient client = new DefaultHttpClient();
-	private String responseBody = null;
-	private HttpResponse response = null;
-	private String failedResponse = "Check Connection or the URL entered.\n<Format: URL - http://www.reddit.com\nParams - key1=value1&key2=value2&...&keyN=valueN>";
+    private HttpClient client = new DefaultHttpClient();
+    private String responseBody = null;
+    private HttpResponse response = null;
+    private String failedResponse = "Check Connection or the URL entered.\n<Format: URL - http://www.reddit.com\nParams - key1=value1&key2=value2&...&keyN=valueN>";
 
-	/**
-	 * HTTP POST Request method.
-	 *
-	 * @param Array of Strings
-	 * @return HTTP Response String
-	 */
-	public String postRequest(String... inputs) {
-		HttpPost post = new HttpPost(inputs[0]);
+    /**
+     * HTTP POST Request method.
+     *
+     * @param Array of Strings
+     * @return HTTP Response String
+     */
+    public String postRequest(String... inputs) {
+        HttpPost post = new HttpPost(inputs[0]);
 
-		try {
-			List<NameValuePair> pairs = new ArrayList<NameValuePair>();
+        try {
+            List<NameValuePair> pairs = new ArrayList<NameValuePair>();
 
-			for (String param: inputs[1].split("&")) {
-				String[] eachPair = null;
-				if (param.contains("=")) {
-					eachPair = param.split("=");
-					pairs.add(new BasicNameValuePair(eachPair[0], eachPair[1]));
-				}
-			}
+            for (String param: inputs[1].split("&")) {
+                String[] eachPair = null;
+                if (param.contains("=")) {
+                    eachPair = param.split("=");
+                    pairs.add(new BasicNameValuePair(eachPair[0], eachPair[1]));
+                }
+            }
 
-			post.setEntity(new UrlEncodedFormEntity(pairs));
+            post.setEntity(new UrlEncodedFormEntity(pairs));
 
-			if (inputs[2].contains(":")) {
-				byte[] encoded = null;
-					encoded = inputs[2].getBytes("UTF-8");
-					String encoding = Base64.encodeToString(encoded, Base64.DEFAULT);
-					post.setHeader("Authorization", "Basic " + encoding);
-			}
+            if (inputs[2].contains(":")) {
+                byte[] encoded = null;
+                    encoded = inputs[2].getBytes("UTF-8");
+                    String encoding = Base64.encodeToString(encoded, Base64.DEFAULT);
+                    post.setHeader("Authorization", "Basic " + encoding);
+            }
 
-			this.response = this.client.execute(post);
-			HttpEntity responseEntity = this.response.getEntity();
+            this.response = this.client.execute(post);
+            HttpEntity responseEntity = this.response.getEntity();
 
-			if (responseEntity != null) {
-				this.responseBody = EntityUtils.toString(responseEntity);
-			}
+            if (responseEntity != null) {
+                this.responseBody = EntityUtils.toString(responseEntity);
+            }
 
         } catch (Exception e) {
             this.responseBody = this.failedResponse;
@@ -67,28 +67,28 @@ public class HttpRequests {
 
         return this.responseBody;
 
-	}
+    }
 
-	/**
-	 * HTTP GET Request method.
-	 *
-	 * @param Array of Strings
-	 * @return HTTP Response String
-	 */
-	public String getRequest(String... inputs) {
-		try {
-			String getURL = inputs[0];
-			HttpGet get = new HttpGet(getURL);
-			this.response = this.client.execute(get);
-			HttpEntity resEntityGet = this.response.getEntity();
-			if (resEntityGet != null) {
-				// do something with the response
-				this.responseBody = EntityUtils.toString(resEntityGet);
-			}
-		} catch (Exception e) {
-			this.responseBody = this.failedResponse;
-		}
-		return this.responseBody;
+    /**
+     * HTTP GET Request method.
+     *
+     * @param Array of Strings
+     * @return HTTP Response String
+     */
+    public String getRequest(String... inputs) {
+        try {
+            String getURL = inputs[0];
+            HttpGet get = new HttpGet(getURL);
+            this.response = this.client.execute(get);
+            HttpEntity resEntityGet = this.response.getEntity();
+            if (resEntityGet != null) {
+                // do something with the response
+                this.responseBody = EntityUtils.toString(resEntityGet);
+            }
+        } catch (Exception e) {
+            this.responseBody = this.failedResponse;
+        }
+        return this.responseBody;
 
-	}
+    }
 }
